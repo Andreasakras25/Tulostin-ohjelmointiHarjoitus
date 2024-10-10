eb415da163dab0fad37d438f0f577560a0f6b063
Andreasakras25 <akrasandreas@gmail.com>
2024-10-10 14:52:44 +0300

    1. Osio
    Ensimmäisessä osiossa ohjelma tulostaa "Hei olen Tulostin-ohjelma" ja "Ohjelman tekijän:".
    Seuraavaksi määritetään String value "opiskelija" ja printataan kyseinen value.

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Printtaa ohjelman "Hei olen Tulostin-ohjelma ja ohjelman tekijän"
+        System.out.println("Hei olen Tulostin-ohjelma");
+        System.out.print("Ohjelman tekijä: ");
+ 
+        // Määritetään String value "opiskelija" ja printataan kyseinen value
+        String tekijä = "opiskelija";
+        System.out.println(tekijä);
+        
+       
